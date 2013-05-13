a8c083424937e625476b8cf52c29eb68a3e426eb
Melissa Wahnish <melissa@aarting.com>
2013-05-13 19:29:53 -0400

    Initial Commit

---
+++ bookmarkletapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarkletapp::Application.config.secret_token = '668bae7ecda0bc553241bbf32e0e1368c4e4f3dca4b02246bdc6a42da027ea7f0e74ffb9c1ddcfe915cad602c9149e52a364c4b55792322a6512bfb937be877a'
